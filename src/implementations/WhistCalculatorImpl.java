0ed3d99a0a381e6b8a99a81c5910ce1af259df1e
Jesper <gedemagt@gmail.com>
2014-03-06 21:41:25 +0100

    Did some start implementations, as JUnit wont work.

--- src/implementations/WhistCalculatorImpl.java
+++ src/implementations/WhistCalculatorImpl.java
@@ -1,5 +1,6 @@
 package implementations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import framework.GameUpdateListener;
@@ -10,28 +11,53 @@ import framework.WhistCalculator;
 
 public class WhistCalculatorImpl implements WhistCalculator {
 
+	private List<GameUpdateListener> listeners;
+	private ScoreCalculator score;
+	private List<PlayerImpl> allPlayers;
+	private List<PlayerImpl> roundPlayers;
+	
 	public WhistCalculatorImpl(ScoreCalculator score) {
-		
+		listeners = new ArrayList<GameUpdateListener>();
+		allPlayers = new ArrayList<PlayerImpl>();
+		this.score = score;
 	}
 	
 	@Override
 	public void endRound(RoundResult result) {
-
+		int[] newScore = score.calculateScore(result, this);
+		setScores(newScore);
+		notifyListeners();
 	}
 
 	@Override
 	public List<Player> getPlayers() {
-		return null;
+		List<Player> players = new ArrayList<Player>();
+		players.addAll(this.allPlayers);
+		return players;
 	}
 
 	@Override
 	public List<Player> getRoundPlayers() {
-		return null;
+		List<Player> players = new ArrayList<Player>();
+		players.addAll(this.roundPlayers);
+		return players;
 	}
 
 	@Override
 	public void registerGameUpdateListener(GameUpdateListener listener) {
-
+		listeners.add(listener);
+	}
+	
+	private void notifyListeners() {
+		for(GameUpdateListener listener : listeners) {
+			listener.onEndOfRound();
+		}
+	}
+	
+	private void setScores(int[] newScores) {
+		for(int i=0; i<newScores.length; i++) {
+			roundPlayers.get(i).setBalance(newScores[i]);
+		}
 	}
 
 }
